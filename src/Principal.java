92f5ee46d2642df984c5c5ae9febab87ec6c3948
Jorge Candeioas do Nascimento <elntele@gmail.com>
2017-10-24 11:16:51 -0300

    falta coverção para km da funçao que calcula distancia entre centroids

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Vector;
@@ -12,7 +13,7 @@ public class Principal {
 		BigListCity bigListCity = new BigListCity(patch);
 		List<City> listCity = bigListCity.getBigListCity();
 		List<Pattern> listPatterns = new ArrayList<>();
-		List<Integer> listIteration = new ArrayList<>();
+		
 
 		/**
 		 * lista de cidades sendo converdida em lista de Pattern
@@ -23,8 +24,10 @@ public class Principal {
 			listPatterns.add(pattern);
 		}
 
-		List<List<Double>> listResult = new ArrayList();
+		List<List<Double>> globalResultSillhouetteAverange = new ArrayList();
 		List<String> listStringCluster = new ArrayList<>();
+		HashMap <Integer, List<Integer>> mapIteration = new HashMap <Integer, List<Integer>>();
+		List<List<Double>> maxMinAverangeDisntance = new ArrayList<>();
 
 		/**
 		 * loop executa o kmeans com k de 4 a 20, ou seja com 4 a 20 clusters ha
@@ -36,10 +39,11 @@ public class Principal {
 		 */
 		for (int i = 4; i <= 20; i++) {
 			List<Pattern> listCentroids = new ArrayList<>();
-			List<Double> result = new ArrayList();
-
+			List<Double> resultSilhouetteAverage = new ArrayList();
+			List<Integer> listIteration = new ArrayList<>();
 			int w = 0;
-			double average = 0;
+			double silhouetteAverage = 0;
+			double DistanceAverange=0;
 			while (w < 30) {
 				// array de listas do tipo pattern sendo preparado para ser
 				// passado
@@ -51,46 +55,66 @@ public class Principal {
 
 				Kmeans kmeans = new Kmeans(i, listPatterns);
 				clustters = kmeans.execute(200);
-				List<String> litleCluster = new ArrayList<>();
-				
+				//List<String> litleCluster = new ArrayList<>();
 				if (w == 29) {
+					// montando a lista com os nomes das cidades de cada cluster
 					String stringCluster = "";
 					for (int u = 0; u < clustters.length; u++) {
-						stringCluster+="{";
+						stringCluster += "{";
 						for (Pattern p : clustters[u]) {
 							stringCluster += p.getName();
 							stringCluster += ", ";
-
 						}
 						stringCluster += "} ";
 					}
 					listStringCluster.add(stringCluster);
+					// calculando a distância mínima, máxima e média entre os centrois
+					Pattern[] centroids = kmeans.getNearestPatternsFromCentroid();
+					double minDistance = Double.MAX_VALUE;
+					double maxDistance = Double.MIN_VALUE;
+					double CurrentDistance = 0;
+					double averageDistance = 0;
+					for (int j = 0; j < centroids.length; j++) {
+						for (int g = j; g < centroids.length; g++) {
+							if (j != g) {
+								CurrentDistance = centroids[j].euclidianDistance(centroids[g]);
+								if (CurrentDistance > maxDistance)
+									maxDistance = CurrentDistance;
+								if (CurrentDistance < minDistance)
+									minDistance = CurrentDistance;
+								DistanceAverange += CurrentDistance;
+							}
+						}
+
+					}
+					List<Double> localMaxMinAverangeDistance = new ArrayList<>();
+					localMaxMinAverangeDistance.add(maxDistance);
+					localMaxMinAverangeDistance.add(minDistance);
+					localMaxMinAverangeDistance.add(DistanceAverange / centroids.length);
+					maxMinAverangeDisntance.add(localMaxMinAverangeDistance);
 				}
 
 				for (int j = 0; j < kmeans.getNearestPatternsFromCentroid().length; j++) {
 					System.out.println(
 							"centroid do cluster" + (j + 1) + kmeans.getNearestPatternsFromCentroid()[j].getName());
 				}
-
-				average += kmeans.getSilhouetteIndex(clustters);
+				silhouetteAverage += kmeans.getSilhouetteIndex(clustters);
 				w += 1;
-				listIteration.addAll(kmeans.getLisItaration());
-				
+				listIteration.add(kmeans.getCountItaration());
 			}
-
-			result.add(average / w);
-			result.add((double) i);
-			listResult.add(result);
-
+			listIteration.sort(null);
+			mapIteration.put(i, listIteration);
+			resultSilhouetteAverage.add(silhouetteAverage / w);
+			resultSilhouetteAverage.add((double) i);
+			globalResultSillhouetteAverange.add(resultSilhouetteAverage);
 		}
 
-
-		listIteration.sort(null);
-		System.out.println(listIteration);
+		
+//		System.out.println(listIteration);
 		
 		
-
-		SpreadSheet dataSheet = new SpreadSheet(listResult, listStringCluster);
-
+		SpreadSheet dataSheet = new SpreadSheet(globalResultSillhouetteAverange, listStringCluster);
+		dataSheet.createSpreedSheetMMAverangeDistance(maxMinAverangeDisntance);
+		dataSheet.createSpreedSheetItereation(mapIteration);
 	}
 }
--- src/SpreadSheet.java
+++ src/SpreadSheet.java
@@ -3,10 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.functions.MinaMaxa;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
@@ -15,9 +17,10 @@ public class SpreadSheet {
 	// Criando o arquivo e uma planilha chamada "Silhouette"
 	
 	private List <List <Double>> clusters = new ArrayList();
-	private static final String fileName = "C:/Users/jorge/workspace/ClusterPe/src/tabela.xls";
-	String clusterString[];
-		
+	private static final String fileNameCsC = "C:/Users/jorge/workspace/ClusterPe/src/cluster_silhouette_cidade .xls";
+	private static final String fileNameIte = "C:/Users/jorge/workspace/ClusterPe/src/Iteratio_MaxMim .xls";
+	private static final String fileNameMiMaAveDis = "C:/Users/jorge/workspace/ClusterPe/src/distanciaDeCentroids .xls";
+	private String clusterString[];
 	public SpreadSheet(List<List<Double>> clusters, List <String> list) {
 		super();
 		this.clusters = clusters;
@@ -32,6 +35,75 @@ public class SpreadSheet {
 		createSpreadSheet();
 
 	}
+	
+//	public SpreadSheet(HashMap <Integer, List<Integer>> mapIteration ) {
+//		super();
+//		createSpreedSheetItereation();
+//	}
+
+
+	
+	public void createSpreedSheetMMAverangeDistance(List<List<Double>> mimMaxAverange ){
+		HSSFWorkbook workTable = new HSSFWorkbook();
+		HSSFSheet sheet = workTable.createSheet("minMaxAverangeDistance");
+		int rownum = 0;
+		int count=0;
+		int j=4;
+		for (List L:mimMaxAverange ){
+        	Row row = sheet.createRow(rownum++);
+            int cellnum = 0;
+            if (count==0){
+            	Cell kNumber = row.createCell(cellnum++);
+            	kNumber.setCellValue("K");
+                Cell distanceMax = row.createCell(cellnum++);
+                distanceMax.setCellValue("max Distance");
+                Cell distanceMin =row.createCell(cellnum++);
+                distanceMin.setCellValue("min Distance");
+                Cell distanceAverange =row.createCell(cellnum++);
+                distanceAverange.setCellValue("Averange distance");
+                Row row2 = sheet.createRow(rownum++);
+                kNumber = row2.createCell(cellnum-4);
+                kNumber.setCellValue(j);
+                distanceMax = row2.createCell(cellnum-3);
+                distanceMax.setCellValue((double)L.get(0));
+                distanceMin = row2.createCell(cellnum-2);
+                distanceMin.setCellValue((double)L.get(1));
+                distanceAverange = row2.createCell(cellnum-1);
+                distanceAverange.setCellValue((double)L.get(2));
+            }else{
+               	Cell kNumber = row.createCell(cellnum++);
+            	kNumber.setCellValue(j);
+                Cell distanceMax = row.createCell(cellnum++);
+                distanceMax.setCellValue((double)L.get(0));
+                Cell distanceMin =row.createCell(cellnum++);
+                distanceMin.setCellValue((double)L.get(1));
+                Cell distanceAverange =row.createCell(cellnum++);
+                distanceAverange.setCellValue((double)L.get(2));
+            }
+            count++;
+            j+=1;
+        }
+         
+        try {
+            FileOutputStream out = 
+                    new FileOutputStream(new File(SpreadSheet.fileNameMiMaAveDis));
+            workTable.write(out);
+            out.close();
+            System.out.println("tabela de distancias criado com sucesso!");
+             
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+               System.out.println("Arquivo não encontrado!");
+        } catch (IOException e) {
+            e.printStackTrace();
+               System.out.println("Erro na edição do arquivo!");
+        }
+
+		
+	
+	}	
+
+	
 	
 	
 	public void createSpreadSheet(){
@@ -72,7 +144,7 @@ public class SpreadSheet {
          
         try {
             FileOutputStream out = 
-                    new FileOutputStream(new File(SpreadSheet.fileName));
+                    new FileOutputStream(new File(SpreadSheet.fileNameCsC));
             workTable.write(out);
             out.close();
             System.out.println("Arquivo Excel criado com sucesso!");
@@ -89,6 +161,57 @@ public class SpreadSheet {
 		this.clusters = clusters;
 	}
 	
+	public void createSpreedSheetItereation(HashMap <Integer, List<Integer>> mapIteration){
+		HSSFWorkbook workTable = new HSSFWorkbook();
+		HSSFSheet sheet = workTable.createSheet("iteration");
+		int rownum = 0;
+		int count=0;
+		for (int j=4;j<=20;j++){
+        	Row row = sheet.createRow(rownum++);
+            int cellnum = 0;
+            if (count==0){
+            	Cell kNumber = row.createCell(cellnum++);
+            	kNumber.setCellValue("K");
+                Cell iterationMax = row.createCell(cellnum++);
+                iterationMax.setCellValue("min iteration");
+                Cell iterationMin =row.createCell(cellnum++);
+                iterationMin.setCellValue("max Interation");
+                Row row2 = sheet.createRow(rownum++);
+                kNumber = row2.createCell(cellnum-3);
+                kNumber.setCellValue(j);
+                iterationMax = row2.createCell(cellnum-2);
+                iterationMax.setCellValue((double)mapIteration.get(j).get(0));
+                iterationMin = row2.createCell(cellnum-1);
+                iterationMin.setCellValue((double)mapIteration.get(j).get(mapIteration.get(j).size()-1));
+            }else{
+               	Cell kNumber = row.createCell(cellnum++);
+            	kNumber.setCellValue(j);
+                Cell iterationMax = row.createCell(cellnum++);
+                iterationMax.setCellValue((double)mapIteration.get(j).get(0));
+                Cell iterationMin =row.createCell(cellnum++);
+                iterationMin.setCellValue((double)mapIteration.get(j).get(mapIteration.get(j).size()-1));
+            }
+            count++;
+        }
+         
+        try {
+            FileOutputStream out = 
+                    new FileOutputStream(new File(SpreadSheet.fileNameIte));
+            workTable.write(out);
+            out.close();
+            System.out.println("tabela de iterações criado com sucesso!");
+             
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+               System.out.println("Arquivo não encontrado!");
+        } catch (IOException e) {
+            e.printStackTrace();
+               System.out.println("Erro na edição do arquivo!");
+        }
+
+		
+	
+	}	
 	
 	
 	
